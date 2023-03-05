4a8f88b4d209b4a5eb1815f85c29987927e68c24
FalejevV <v.o.v.a@hotmail.com>
2023-03-05 19:33:54 +0200

    No more Orientation-lock?

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import SearchBar from './components/SearchBar/SearchBar';
 import { MainView } from './styles/Styled.styled';
-
+import * as ScreenOrientation from 'expo-screen-orientation'
+ScreenOrientation.unlockAsync();
 
 export default function App() {
   return (
